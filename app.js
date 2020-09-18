4fe8f4f7a948adca75894dbd52356ca1592f73d9
imshubham <jangleshubham@ymail.com>
2020-09-18 21:09:19 +0530

    Added delete and some sanitization

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	methodOverride = require("method-override"),
 	mongoose = require("mongoose"),
+	expressSanitizer = require("express-sanitizer"),
 	app = express();
 
 mongoose.connect('mongodb://localhost:27017/restful_blog_app',  {
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/restful_blog_app',  {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 
@@ -51,6 +53,7 @@ app.get("/blogs/new", function(req, res){
 
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body); //remove any unreq code in body
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){ res.render("new") }
 		else{ res.redirect("/blogs") }
@@ -83,8 +86,21 @@ app.get("/blogs/:id/edit", function(req, res){
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		req.body.blog.body = req.sanitize(req.body.blog.body); //remove any unreq code in body
 		if(err){ res.redirect("/blogs") }
 		else { res.redirect("/blogs/" + req.params.id); }
+	})
+});
+
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
 	})
 });
 
